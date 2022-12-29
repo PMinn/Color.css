d5661d7b5ae2f0a37997147fefd1012cee281cb0
AlanPan0131 <panalan0131@gmail.com>
2022-12-30 00:47:38 +0800

    update

--- src/render.js
+++ src/render.js
@@ -10,25 +10,33 @@ function renderColorCSS(name, colors) {
     fs.writeFileSync(`./${name}.css`, css);
 }
 
-function renderColorHTML(color) {
+function renderColorHTML(nav, color) {
     ejs.renderFile('./color.ejs', { nav, data: color }, {}, function (err, html) {
         fs.writeFileSync(`../${color.id}.html`, html, 'utf8');
     });
 }
 
-function renderIndexHTML() {
+function renderIndexHTML(nav, data) {
+    var now = new Date();
+    var mm = now.getMonth() + 1; // getMonth() is zero-based
+    var dd = now.getDate();
+
+    ejs.renderFile('./docs.ejs', {
+        nav,
+        dateString: [now.getFullYear(), (mm > 9 ? '' : '0') + mm, (dd > 9 ? '' : '0') + dd].join('-'),
+        data
+    }, {}, function (err, html) {
+        fs.writeFileSync(`../docs.html`, html, 'utf8');
+    });
+}
+
+function renderGetStartedHTML(nav) {
     ejs.renderFile('./get-started.ejs', { nav }, {}, function (err, html) {
         fs.writeFileSync(`../get-started.html`, html, 'utf8');
     });
 }
 
-function renderGetStartedHTML() {
-    ejs.renderFile('./index.ejs', { nav }, {}, function (err, html) {
-        fs.writeFileSync(`../index.html`, html, 'utf8');
-    });
-}
-
-function renderREADME() {
+function renderREADME(data) {
     ejs.renderFile('./README.ejs', { data }, {}, function (err, html) {
         fs.writeFileSync(`../README.md`, html, 'utf8');
     });
@@ -37,15 +45,16 @@ function renderREADME() {
 const data = JSON.parse(fs.readFileSync('../data.json'));
 const nav = data.map(c => ({ id: c.id, name: c.name }));
 // console.log(data);
-data.forEach(color => {
-    try {
-        renderColorCSS(color.id, color.data);
-        renderColorHTML(color);
-    } catch (e) {
-        console.log(e);
-    }
-})
 
-renderIndexHTML();
-renderGetStartedHTML();
-renderREADME();
+// data.forEach(color => {
+//     try {
+//         renderColorCSS(color.id, color.data);
+//         renderColorHTML(nav, color);
+//     } catch (e) {
+//         console.log(e);
+//     }
+// })
+
+renderIndexHTML(nav, data);
+// renderGetStartedHTML(nav);
+// renderREADME(data);
